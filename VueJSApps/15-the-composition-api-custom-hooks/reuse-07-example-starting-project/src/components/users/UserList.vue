0fa99faca8a89fbaf5fa0418c32811235b4e5a75
Dimitar Glavyanov <Dimitar.Glavyanov@kontrax.bg>
2023-04-07 17:36:34 +0300

    add sort hook

--- VueJSApps/15-the-composition-api-custom-hooks/reuse-07-example-starting-project/src/components/users/UserList.vue
+++ VueJSApps/15-the-composition-api-custom-hooks/reuse-07-example-starting-project/src/components/users/UserList.vue
@@ -26,8 +26,9 @@
 </template>
 
 <script>
-import { ref, computed, toRefs } from 'vue';
+import { toRefs } from 'vue';
 import useSearch from '../../hooks/search';
+import useSort from '../../hooks/sort';
 
 import UserItem from './UserItem.vue';
 
@@ -64,35 +65,17 @@ export default {
     function updateSearch(val) {
       enteredSearchTerm.value = val;
     } */
-    const {users} = toRefs(props);
+    const { users } = toRefs(props);
 
-    const {
-      updateSearch,
+    const { updateSearch, availableItems, enteredSearchTerm } = useSearch(
+      users,
+      'fullName'
+    );
+
+    const { displayedUsers, sort, sorting } = useSort(
       availableItems,
-      enteredSearchTerm,
-    } = useSearch(users, 'fullName');
-
-    const sorting = ref(null);
-    const displayedUsers = computed(function () {
-      if (!sorting.value) {
-        return availableItems.value;
-      }
-      return availableItems.value.slice().sort((u1, u2) => {
-        if (sorting.value === 'asc' && u1.fullName > u2.fullName) {
-          return 1;
-        } else if (sorting.value === 'asc') {
-          return -1;
-        } else if (sorting.value === 'desc' && u1.fullName > u2.fullName) {
-          return -1;
-        } else {
-          return 1;
-        }
-      });
-    });
-
-    function sort(mode) {
-      sorting.value = mode;
-    }
+      'fullName'
+    );
 
     return {
       enteredSearchTerm,
