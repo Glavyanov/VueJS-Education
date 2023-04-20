3cb95ad689048a83fc07264fa147f6084de84e9d
Dimitar Glavyanov <Dimitar.Glavyanov@kontrax.bg>
2023-04-20 17:38:03 +0300

    update AppView

--- VueJSFirstDemo/vue-school-travel-app/src/App.vue
+++ VueJSFirstDemo/vue-school-travel-app/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
     <router-link to="/brazil">Brazil</router-link>
   </div>
   <div class="container">
