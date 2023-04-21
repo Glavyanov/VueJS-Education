de898f756a1e7077e779510a1ea01878959e030a
Dimitar Glavyanov <Dimitar.Glavyanov@kontrax.bg>
2023-04-21 09:33:58 +0300

    add destinations

--- VueJSFirstDemo/vue-school-travel-app/src/App.vue
+++ VueJSFirstDemo/vue-school-travel-app/src/App.vue
@@ -3,6 +3,9 @@
     <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
     <router-link to="/brazil">Brazil</router-link>
+    <router-link to="/hawaii">Hawaii</router-link>
+    <router-link to="/jamaica">Jamaica</router-link>
+    <router-link to="/panama">Panama</router-link>
   </div>
   <div class="container">
     <router-view></router-view>
--- VueJSFirstDemo/vue-school-travel-app/src/views/HomeView.vue
+++ VueJSFirstDemo/vue-school-travel-app/src/views/HomeView.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="home">
-    <h1>HomeView</h1>
+    <h1>All Destinations</h1>
+    <div class="destinations">
+      <router-link
+        v-for="destination in destinations"
+        :key="destination.id"
+        :to="destination.slug"
+      >
+        {{ destination.name }}
+        <img :src="`/images/${destination.image}`" :alt="destination.name" />
+      </router-link>
+    </div>
   </div>
 </template>
+
+<script setup>
+import sourceData from "@/data.json";
+const destinations = sourceData.destinations;
+</script>
