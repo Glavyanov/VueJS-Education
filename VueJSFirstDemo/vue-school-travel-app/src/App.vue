ce8406adf3ffeee2f2c22c4fed84ae484d6b5802
Dimitar Glavyanov <Dimitar.Glavyanov@kontrax.bg>
2023-04-21 10:19:40 +0300

    separate navigation

--- VueJSFirstDemo/vue-school-travel-app/src/App.vue
+++ VueJSFirstDemo/vue-school-travel-app/src/App.vue
@@ -1,16 +1,13 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
-    <router-link to="/brazil">Brazil</router-link>
-    <router-link to="/hawaii">Hawaii</router-link>
-    <router-link to="/jamaica">Jamaica</router-link>
-    <router-link to="/panama">Panama</router-link>
-  </div>
+  <TheNavigation />
   <div class="container">
     <router-view></router-view>
   </div>
 </template>
+
+<script setup>
+import TheNavigation from "@/components/TheNavigation.vue";
+</script>
 
 <style>
 /* #app {
