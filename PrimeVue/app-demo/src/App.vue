55c10d54bf078cf6538e59e3c57ca7322650c2f1
Glavyanov <dimitarglavianov@gmail.com>
2023-04-25 00:14:14 +0300

    add Change Theme

--- PrimeVue/app-demo/src/App.vue
+++ PrimeVue/app-demo/src/App.vue
@@ -1,25 +1,3 @@
-<script setup>
-import { ref } from "vue";
-import { useToast } from "primevue/usetoast";
-
-const text = ref();
-const autoCom = ref("");
-
-const toast = useToast();
-const items = ref([]);
-
-const greet = () => {
-  console.log("FromToast");
-  toast.add({ severity: "success", summary: autoCom.value , detail: text.value, });
-  console.log(toast);
-};
-
-const search = (event) => {
-    let _items = [...Array(10).keys()];
-    items.value = event.query ? [...Array(10).keys()].map((item) => event.query + '-' + item) : _items;
-}
-</script>
-
 <template>
   <ToastBase></ToastBase>
   <div class="container">
@@ -27,10 +5,76 @@ const search = (event) => {
       <InputText id="txt" v-model="text" />
       <label for="txt">Text</label>
     </span>
-    <ButtonBase label="Greet" @click="greet" icon="pi pi-user" style="margin-left: 20px; height: 50px;"></ButtonBase>
-    <AutoComplete id="autocom" v-model="autoCom" dropdown :suggestions="items"  @complete="search" ></AutoComplete>
+    <ButtonBase
+      label="Greet"
+      @click="greet"
+      icon="pi pi-user"
+      style="margin-left: 20px; height: 50px"
+    ></ButtonBase>
+    <AutoComplete
+      id="autocom"
+      v-model="autoCom"
+      dropdown
+      :suggestions="items"
+      @complete="search"
+    ></AutoComplete>
   </div>
+  <ButtonBase
+    label="Change Theme"
+    @click="isDark = !isDark"
+    :icon="icon"
+    style="margin-left: 20px; height: 50px"
+  ></ButtonBase>
 </template>
+
+<script setup>
+import { ref, watch } from "vue";
+import { useToast } from "primevue/usetoast";
+import { usePrimeVue } from "primevue/config";
+
+const PrimeVue = usePrimeVue();
+
+const text = ref();
+const icon = ref("pi pi-moon");
+const autoCom = ref("");
+const isDark = ref(false);
+
+const toast = useToast();
+const items = ref([]);
+
+const greet = () => {
+  toast.add({
+    severity: "success",
+    summary: autoCom.value,
+    detail: text.value,
+  });
+};
+
+const search = (event) => {
+  let _items = [...Array(10).keys()];
+  items.value = event.query
+    ? [...Array(10).keys()].map((item) => event.query + "-" + item)
+    : _items;
+};
+
+watch(isDark, () => {
+  if (isDark.value) {
+    PrimeVue.changeTheme(
+      "lara-light-purple",
+      "lara-dark-indigo",
+      "id-to-link",
+      () => (icon.value = "pi pi-sun")
+    );
+  } else {
+    PrimeVue.changeTheme(
+      "lara-dark-indigo",
+      "lara-light-purple",
+      "id-to-link",
+      () => (icon.value = "pi pi-moon")
+    );
+  }
+});
+</script>
 
 <style>
 .container {
