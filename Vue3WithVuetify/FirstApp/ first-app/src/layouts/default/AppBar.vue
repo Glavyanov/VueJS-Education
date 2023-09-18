1ad4d97fa31dc4129d3b97651125d0f98babcac8
Dimitar Glavyanov <Dimitar.Glavyanov@kontrax.bg>
2023-09-18 18:29:35 +0300

    add vuetify app

---
+++ Vue3WithVuetify/FirstApp/ first-app/src/layouts/default/AppBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app-bar rounded :elevation="16" :collapse="collapse">
+    <v-icon icon="mdi-circle-slice-6" @click="collapse = !collapse" class="ml-4"/>
+    <v-spacer></v-spacer>
+    <v-app-bar-title>
+      <section>
+        <h1 style="display: inline-block;">{{ header }}</h1>
+      </section>
+    </v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script setup>
+  //
+  import { ref } from 'vue';
+  const header = ref("Vuetify app");
+  const collapse = ref(false);
+</script>
