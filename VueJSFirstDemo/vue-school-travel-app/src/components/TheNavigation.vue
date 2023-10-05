86ee8cd7c82dda55b52a06c091e1ff54ce9a2d13
Dimitar Glavyanov <Dimitar.Glavyanov@kontrax.bg>
2023-10-05 11:51:47 +0300

    add travel app again

---
+++ VueJSFirstDemo/vue-school-travel-app/src/components/TheNavigation.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="nav">
+    <router-link id="logo" to="/">Vue School Travel</router-link>
+    <router-link
+      v-for="destination in destinations"
+      :key="destination.id"
+      :to="{
+        name: 'destination.show',
+        params: { id: destination.id, slug: destination.slug },
+      }"
+    >
+      {{ destination.name }}
+    </router-link>
+  </div>
+</template>
+
+<script setup>
+import sourceData from "@/data.json";
+const destinations = sourceData.destinations;
+</script>
+
+<style scoped>
+#nav .vue-school-active-link {
+  color: rgb(78, 162, 196);
+  border-bottom: 2px solid rgb(78, 162, 196);
+}
+</style>
---
+++ VueJSFirstDemo/vue-school-travel-app/src/router/index.js
@@ -0,0 +1,88 @@
+import { createRouter, createWebHistory } from "vue-router";
+import sourceData from "@/data.json";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: () =>
+      import(/* webpackChunkName: "home-view" */ "@/views/HomeView.vue"),
+  },
+
+  {
+    path: "/destination/:id/:slug",
+    name: "destination.show",
+    component: () =>
+      import(
+        /* webpackChunkName: "destination-show" */ "@/views/DestinationShow.vue"
+      ),
+    props: (route) => ({ ...route.params, id: parseInt(route.params.id) }),
+    beforeEnter(to) {
+      const exist = sourceData.destinations.find(
+        (dest) =>
+          dest.id === parseInt(to.params.id) && dest.slug === to.params.slug
+      );
+
+      if (!exist)
+        return {
+          name: "not-found",
+          query: { url: to.path },
+          hash: to.hash,
+        };
+    },
+    children: [
+      {
+        path: "/destination/:id/:slug/:experienceSlug",
+        name: "experience.show",
+        component: () =>
+          import(
+            /* webpackChunkName: "experience-show" */ "@/views/ExperienceShow.vue"
+          ),
+        props: (route) => ({ ...route.params, id: parseInt(route.params.id) }),
+        beforeEnter(to) {
+          const exist = sourceData.destinations.find(
+            (dest) =>
+              dest.id === parseInt(to.params.id) &&
+              dest.experiences.some((d) => d.slug === to.params.experienceSlug)
+          );
+
+          if (!exist)
+            return {
+              name: "not-found",
+              query: { url: to.path },
+              hash: to.hash,
+            };
+        },
+        children: [
+          {
+            path: ":notFound(.*)*",
+            name: "not-found",
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    name: "not-found",
+    path: "/:notFound(.*)*",
+    component: () => import("@/components/NotFound.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+  linkActiveClass: "vue-school-active-link",
+});
+
+export default router;
+// {
+//   path: "/about",
+//   name: "about",
+//   // route level code-splitting
+//   // this generates a separate chunk (about.[hash].js) for this route
+//   // which is lazy-loaded when the route is visited.
+//   component: () =>
+//     import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+// },
