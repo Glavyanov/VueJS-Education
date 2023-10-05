66ef98a71fcf948378c4dad345e24e8d10303b3e
Dimitar Glavyanov <Dimitar.Glavyanov@kontrax.bg>
2023-10-05 16:34:06 +0300

    Add cards

--- PrimeVue/app-demo/src/components/TaskDetails.vue
+++ PrimeVue/app-demo/src/components/TaskDetails.vue
@@ -1,9 +1,38 @@
 <template>
-  <h3>{{ task.title }}</h3>
+  <Card style="width: 400px">
+    <template #header>
+    </template>
+    <template #title>{{ task.title }}</template>
+    <template #subtitle> Card subtitle </template>
+    <template #content>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed
+        consequuntur error repudiandae numquam deserunt quisquam repellat libero
+        asperiores earum nam nobis, culpa ratione quam perferendis esse,
+        cupiditate neque quas!
+      </p>
+    </template>
+    <template #footer>
+      <ButtonBase icon="pi pi-check" label="Save" />
+      <ButtonBase
+        icon="pi pi-times"
+        label="Cancel"
+        severity="secondary"
+        style="margin-left: 0.5em"
+      />
+      <span style="margin-left: 25px">
+        <i class="pi pi-fw pi-trash"></i>
+        <i class="pi pi-spin pi-spinner" />
+        <i class="pi pi-spin pi-cog" />
+      </span>
+    </template>
+  </Card>
 </template>
 
 <script setup>
 import { defineProps } from "vue";
+import Card from "primevue/card";
+
 defineProps({
   task: Object,
 });
--- PrimeVue/app-demo/src/store/taskStore.js
+++ PrimeVue/app-demo/src/store/taskStore.js
@@ -4,7 +4,7 @@ import { ref } from 'vue';
 export const useTaskStore = defineStore('taskStore', () => {
 
     const tasks = ref([
-        {id: 1, title: "buy some apple...", isFav: false},
+        {id: 1, title: "buy some apple...", isFav: false },
         {id: 2, title: "buy that pinia...", isFav: true},
     ]);
 
