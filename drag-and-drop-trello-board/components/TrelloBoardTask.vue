1f586a9f6ea9df4281c4c5f04dea621c1c167c82
Dimitar Glavyanov <dimitarglavianov@gmail.com>
2024-01-12 21:47:05 +0000

    added simple layout

---
+++ drag-and-drop-trello-board/components/TrelloBoardTask.vue
@@ -0,0 +1,15 @@
+<template>
+    <div 
+        :title="task.createdAt.toLocaleDateString()" 
+        class="task bg-white p-2 mb-2 rounded shadow-sm max-w-[250px]"
+    >
+        <span>{{ task.title }}</span>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { Task } from "../types";
+defineProps<{
+    task: Task;
+}>();
+</script>
