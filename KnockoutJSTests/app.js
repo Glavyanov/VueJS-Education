b70890b1a50121365c84c69643a1d3fffa1aaa81
Glavyanov <dimitarglavianov@gmail.com>
2024-05-03 05:00:55 +0300

    Add some KnockoutJS tests.

---
+++ KnockoutJSTests/app.js
@@ -0,0 +1,21 @@
+// This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
+function AppViewModel() {
+    var that = this;
+    that.firstName = ko.observable("Bert");
+    that.lastName = ko.observable("Bertington");
+    that.friends = ko.observableArray([new Friend("Scott"), new Friend("Annie")]);
+    that.addFriend = function(){
+        that.friends.unshift(new Friend());
+    };
+    
+    function Friend(name){
+        var self = this;
+        self.name = ko.observable(name);
+        self.remove = function(friend){
+            this.friends.remove(friend);
+        }.bind(that);
+    }
+}
+
+// Activates knockout.js
+ko.applyBindings(new AppViewModel());
