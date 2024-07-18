66b0d7de9e7d670a23b7f6110f739301643a5fef
Dimitar Glavyanov <Dimitar.Glavyanov@kontrax.bg>
2024-07-18 13:59:07 +0300

    final with typescript

--- vite-course/src/App.vue
+++ vite-course/src/App.vue
@@ -1,23 +1,24 @@
-<script setup>
+<script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from "./components/HelloWorld.vue";
-import styles from "./assets/example.module.css";
+import { ref } from "vue";
+import HelloWorld from "@/components/HelloWorld.vue";
+import styles from "@/assets/example.module.css";
+import data from "@/data.json";
+import { developerMood } from "@/data.json";
+
+console.log(data);
+console.log(developerMood);
+console.log(import.meta.env);
+
+const images = import.meta.globEager("./assets/avatars/*.jpg");
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <h1 :class="styles.example">Hello CSS Modules</h1>
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <img alt="Vue logo" src="@/assets/tiny-vue-logo.png" />
+  <h1 :class="styles.example">CSS Modules</h1>
+  <HelloWorld msg="Vue 3 + Vite" />
+  <div>
+    <img v-for="image in images" :key="image" :src="image.default" alt="img" />
+  </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
